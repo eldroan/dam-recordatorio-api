82a787db4f0e4c0dc5e247175696417070510156
Leandro Amarillo <leandroamarillo95@gmail.com>
2021-11-05 21:41:01 -0300

    Added helper shell comand to change credentials

---
+++ replace.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Assign the filename
+compose="docker-compose.yml"
+config="config/config.json"
+
+read -p "Enter the DB_NAME: " db_name
+read -p "Enter the DB_USER: " db_user
+read -p "Enter the DB_PASS: " db_pass
+
+if [[ $db_name != "" && $db_user != "" && $db_pass != "" ]]; then
+sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $compose
+sed -i '' "s/DEFAULT_DB_USER/$db_user/" $compose
+sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $compose
+sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $config
+sed -i '' "s/DEFAULT_DB_USER/$db_user/" $config
+sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $config
+fi
