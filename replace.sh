c1caac61a661c06e8e46231f4107e522d77ad26b
Leandro Amarillo <leandroamarillo95@gmail.com>
2021-11-05 22:00:25 -0300

    Linux support

--- replace.sh
+++ replace.sh
@@ -9,10 +9,19 @@ read -p "Enter the DB_USER: " db_user
 read -p "Enter the DB_PASS: " db_pass
 
 if [[ $db_name != "" && $db_user != "" && $db_pass != "" ]]; then
-sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $compose
-sed -i '' "s/DEFAULT_DB_USER/$db_user/" $compose
-sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $compose
-sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $config
-sed -i '' "s/DEFAULT_DB_USER/$db_user/" $config
-sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $config
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $compose
+        sed -i '' "s/DEFAULT_DB_USER/$db_user/" $compose
+        sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $compose
+        sed -i '' "s/DEFAULT_DB_NAME/$db_name/" $config
+        sed -i '' "s/DEFAULT_DB_USER/$db_user/" $config
+        sed -i '' "s/DEFAULT_DB_PASS/$db_pass/" $config
+    else
+        sed -i "s/DEFAULT_DB_NAME/$db_name/" $compose
+        sed -i "s/DEFAULT_DB_USER/$db_user/" $compose
+        sed -i "s/DEFAULT_DB_PASS/$db_pass/" $compose
+        sed -i "s/DEFAULT_DB_NAME/$db_name/" $config
+        sed -i "s/DEFAULT_DB_USER/$db_user/" $config
+        sed -i "s/DEFAULT_DB_PASS/$db_pass/" $config
+    fi
 fi
